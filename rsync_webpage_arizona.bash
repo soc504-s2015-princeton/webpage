5a2b22ac63ec39bfc1da41400d557f75af3dd283
Matthew Salganik <msalganik@yahoo.com>
2015-01-26 09:46:19 -0500

    cleaning up permissions

--- rsync_webpage_arizona.bash
+++ rsync_webpage_arizona.bash
@@ -3,4 +3,4 @@
 # -a: archive
 # -z: enables compression
 
-rsync -apmvz --exclude=.git/ --exclude=*.bash# * mjs3@arizona.princeton.edu:~/public_html/soc504_s2015
+rsync -apmvz --exclude=.git/ --exclude=*.bash* * mjs3@arizona.princeton.edu:~/public_html/soc504_s2015
